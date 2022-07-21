53cb0c43c220023b0ec3c1cabaec3c6ad540059d
Amr AbdulSalam <amrsalam@hotmail.com>
2022-07-21 11:54:08 +0300

    cheeck valid username & password

--- src/Client.java
+++ src/Client.java
@@ -3,27 +3,27 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		JFrame frame = new JFrame("Login-Page");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel("Inventory System");  
-        JButton button = new JButton();  
-        button.setText("Button");  
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(900, 600);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
-		
+		 try  
+	        {  
+	            //create instance of the CreateLoginForm  
+	            CreateLoginForm form = new CreateLoginForm();  
+	            form.setSize(600,400);  //set size of the frame  
+	            form.setLocationRelativeTo(null);  
+	            form.setVisible(true);  //make form visible to the user  
+	        }  
+	        catch(Exception e)  
+	        {     
+	            //handle exception   
+	            JOptionPane.showMessageDialog(null, e.getMessage());  
+	        }  
 	}
 
 }
