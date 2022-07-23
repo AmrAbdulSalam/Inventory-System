0dd3a7cfdaf4f816e5f92362f989e4e2d2d3386f
Amr AbdulSalam <amrsalam@hotmail.com>
2022-07-23 16:19:47 +0300

    GUI Ready

--- src/NewPage.java
+++ src/NewPage.java
@@ -12,8 +12,11 @@ class NewPage extends JFrame
         WindowConstants.DISPOSE_ON_CLOSE);  
         setTitle("Inventory");  
         setSize(900, 600);  
+        JTable j;
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
+        JPanel p22 = new JPanel();
+        JPanel p23 = new JPanel();
         JPanel p3 = new JPanel();
         JLabel product_id = new JLabel("ProductID");
         JTextField product = new JTextField(15);
@@ -25,19 +28,49 @@ class NewPage extends JFrame
         
         JLabel product_id2 = new JLabel("ProductID");
         JTextField product2 = new JTextField(15);
-        JLabel new_value = new JLabel("Value");
-        JTextField value = new JTextField(15);
-        JButton set_product = new JButton("SetProduct");
+        JLabel count = new JLabel("Count");
+        JTextField count_text = new JTextField(15);
+        JLabel product_price = new JLabel("Price");
+        JTextField price = new JTextField(15);
+        JLabel product_name = new JLabel("Name");
+        JTextField name = new JTextField(15);
+        JButton set_product = new JButton("AddProduct");
+        JButton update = new JButton("UpdateProduct");
+        
+        String[][] data = {
+                { "Kundan Kumar Jha", "4031", "CSE"  , "500"},
+                { "Anand Jha", "6014", "IT" , "200" }
+            };
+     
+            // Column Names
+            String[] columnNames = { "ID", "Name", "Price"  , "Count"};
+     
+            // Initializing the JTable
+            j = new JTable(data, columnNames);
+            j.setBounds(30, 40, 200, 300);
+     
+            // adding it to JScrollPane
+            JScrollPane sp = new JScrollPane(j);
+        
         
         p2.add(product_id2);
         p2.add(product2);
-        p2.add(new_value);
-        p2.add(value);
-        p2.add(set_product);
+        p2.add(count);
+        p2.add(count_text);
+        p2.add(product_price);
+        p2.add(price);
+        p22.add(product_name);
+        p22.add(name);
+        p22.add(set_product);
+        p22.add(update);
+        p23.add(sp);
+        
+        
         
         p3.add(p1);
         p3.add(p2);
-        
+        p3.add(p22);
+        p3.add(p23);
         add(p3);
         
         
